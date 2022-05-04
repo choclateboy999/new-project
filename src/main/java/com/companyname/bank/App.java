891770194b7dfe07588c5907bbd54ff612093c71
choclateboy <103100764+choclateboy999@users.noreply.github.com>
2022-05-04 14:07:33 +0530

    eeettttt new

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World wellcome to rajeshkumar automation!" );
+        System.out.println( "Hello World wellcome to  wwwwww!" );
     }
 }
