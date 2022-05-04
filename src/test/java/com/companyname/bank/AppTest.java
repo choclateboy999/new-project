e35066670d28c1f102fadb2f97c7cbf311cc17b5
choclateboy <rajeshkumar.insider@gmail.com>
2022-05-04 12:54:10 +0530

    new cdci changes

---
+++ src/test/java/com/companyname/bank/AppTest.java
@@ -0,0 +1,12 @@
+package com.companyname.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
