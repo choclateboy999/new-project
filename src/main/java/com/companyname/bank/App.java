f85458c27ca5b237b1d1b65aaecc49433fe40fe0
choclateboy <103100764+choclateboy999@users.noreply.github.com>
2022-05-05 16:19:47 +0530

    rajeshkumar changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World wellcome to kkkkk!" );
+        System.out.println( "Hello World wellcome to mr rajeshkumar!" );
     }
 }
